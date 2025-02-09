92061a1865135dba30741fd2f7b71e4a18c3f271
Mateus Kasuya <mateusvbkasuya@gmail.com>
2025-02-09 11:36:45 -0300

    insert nba teams no mongodb

---
+++ src/mongodb/insert.py
@@ -0,0 +1,22 @@
+from pymongo import MongoClient
+
+def insert_to_mongodb(uri: str, database: str, collection: str, json: dict) -> None: 
+
+    try:
+            client = MongoClient(uri)
+
+            database = client[database]
+            collection = database[collection]
+
+            document_list = [json]
+            result = collection.insert_many(document_list)
+            
+            print(result.acknowledged)
+
+            client.close()
+
+    except Exception as e:
+        raise Exception(
+            "The following error occurred: ", e)
+    
+    pass
