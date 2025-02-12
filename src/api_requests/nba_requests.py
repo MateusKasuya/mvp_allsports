4c8d2803557cff515cc92f16bdffbcb16882a588
Mateus Kasuya <mateusvbkasuya@gmail.com>
2025-02-12 14:01:39 -0300

    fix: nba api quota exceeded

--- src/api_requests/nba_requests.py
+++ src/api_requests/nba_requests.py
@@ -22,7 +22,7 @@ def request_nba_team_profile(
     language_code: str,
     format: str,
     api_key: str,
-):
+) -> list:
 
     team_ids = [
         team['id']
@@ -48,3 +48,34 @@ def request_nba_team_profile(
         time.sleep(5)
 
     return list_of_teams
+
+
+def nba_requests_player_profile(
+    teams: list,
+    access_level: str,
+    language_code: str,
+    format: str,
+    api_key: str,
+) -> list:
+
+    player_ids = [
+        player['id'] for team in teams for player in team.get('players', [])
+    ]
+
+    list_of_players = []
+
+    for player_id in player_ids:
+
+        url = f'https://api.sportradar.com/nba/{access_level}/v8/{language_code}/players/{player_id}/profile.{format}?api_key={api_key}'
+
+        headers = {'accept': 'application/json'}
+
+        response = requests.get(url, headers=headers)
+
+        data = response.json()
+
+        list_of_players.append(data)
+
+        time.sleep(5)
+
+    return list_of_players
--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from dotenv import dotenv_values
 
 from src.api_requests.nba_requests import (
+    nba_requests_player_profile,
     request_nba_league_hierarchy,
     request_nba_team_profile,
 )
@@ -25,7 +26,7 @@ features = {
     'mongodb': {
         'uri': settings['MONGOURI'],
         'database': 'nba',
-        'collection': ['league_hierarchy', 'team_profile'],
+        'collection': ['league_hierarchy', 'team_profile', 'player_profile'],
     },
 }
 
@@ -42,23 +43,44 @@ if __name__ == '__main__':
     #     collection=features['mongodb']['collection'][0],
     #     json=league_hierarchy,
     # )
-    teams = query_mongodb(
-        uri=features['mongodb']['uri'],
-        database=features['mongodb']['database'],
-        collection=features['mongodb']['collection'][0],
-        query={},
-    )
-    team_profile = request_nba_team_profile(
-        teams,
-        access_level=features['api_request']['access_level'],
-        language_code=features['api_request']['language_code'],
-        format=features['api_request']['format'],
-        api_key=features['api_request']['api_key'],
-    )
-    for team in team_profile:
-        load_to_mongodb(
-            uri=features['mongodb']['uri'],
-            database=features['mongodb']['database'],
-            collection=features['mongodb']['collection'][1],
-            json=team,
-        )
+    # teams = query_mongodb(
+    #     uri=features['mongodb']['uri'],
+    #     database=features['mongodb']['database'],
+    #     collection=features['mongodb']['collection'][0],
+    #     query={},
+    # )
+    # team_profile = request_nba_team_profile(
+    #     teams,
+    #     access_level=features['api_request']['access_level'],
+    #     language_code=features['api_request']['language_code'],
+    #     format=features['api_request']['format'],
+    #     api_key=features['api_request']['api_key'],
+    # )
+    # for team in team_profile:
+    #     load_to_mongodb(
+    #         uri=features['mongodb']['uri'],
+    #         database=features['mongodb']['database'],
+    #         collection=features['mongodb']['collection'][1],
+    #         json=team,
+    #     )
+    # players_query = query_mongodb(
+    #     uri=features['mongodb']['uri'],
+    #     database=features['mongodb']['database'],
+    #     collection=features['mongodb']['collection'][1],
+    #     query={},
+    # )
+    # player_profile = nba_requests_player_profile(
+    #     players_query,
+    #     access_level=features['api_request']['access_level'],
+    #     language_code=features['api_request']['language_code'],
+    #     format=features['api_request']['format'],
+    #     api_key=features['api_request']['api_key'],
+    # )
+    # for player in player_profile:
+    #     load_to_mongodb(
+    #         uri=features['mongodb']['uri'],
+    #         database=features['mongodb']['database'],
+    #         collection=features['mongodb']['collection'][2],
+    #         json=player,
+    #     )
+    pass
