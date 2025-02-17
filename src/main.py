9a9355236e12a9840a37495f904fd9f1d4e0b9da
Mateus Kasuya <mateusvbkasuya@gmail.com>
2025-02-17 18:00:56 -0300

    build: check if exists

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
-from dotenv import load_dotenv
-import sys
 import os
+import sys
+
+from dotenv import load_dotenv
 
 # Adiciona o diretório 'src' ao sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -9,13 +10,19 @@ from src.utils.player_props_pipeline import PlayerPropsPipeline
 
 load_dotenv()
 
-#uri = "mongodb://localhost:27017/"
-uri = os.getenv("MONGOURI")
-api_key = os.getenv("APIKEY")
+uri = 'mongodb://localhost:27017/'
+# uri = os.getenv('MONGOURI')
+api_key = os.getenv('APIKEY')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-    player_props = PlayerPropsPipeline(uri= uri, database= "oddsplayerprops", collection= "sports", access_level= "trial", api_key= api_key)
+    player_props = PlayerPropsPipeline(
+        uri=uri,
+        database='oddsplayerprops',
+        collection='sports',
+        access_level='trial',
+        api_key=api_key,
+    )
 
     player_props.sports_pipeline()
 
